d8eabf89965a3434f3dadf75d7051693929e68dc
juliegbblz <zilberbergjulie@gmail.com>
2026-03-23 11:04:56 +0100

    function update

--- premier.py
+++ premier.py
@@ -1,7 +1,5 @@
 import random
 
-from sympy import true
-
 from euclide_etendu import exponentiation_modulaire
 
 
@@ -10,21 +8,39 @@ def generation_nombre_premier(nbits):
     return premier
 
 def temoin(a,premier):
-    x[0]=exponentiation_modulaire(a,u,premier)
-    for i in range(1,t):
-        x[i]=(x[i-1]**2)%premier
-        if x[i]==1 and x[i-1]!=1 and x[i-1]!=premier-1:
+    if premier < 4:
+        return False
+
+    u = premier - 1
+    t = 0
+    while u % 2 == 0:
+        u //= 2
+        t += 1
+
+    x = [0] * (t + 1)
+    x[0] = exponentiation_modulaire(a, u, premier)
+    if x[0] == 1 or x[0] == premier - 1:
+        return False
+
+    for i in range(1, t + 1):
+        x[i] = (x[i - 1] ** 2) % premier
+        if x[i] == premier - 1:
+            return False
+        if x[i] == 1 and x[i - 1] != 1 and x[i - 1] != premier - 1:
             return True
-    if x[t]!=1:
-        return True
-    return False
+
+    return True
 
 def pseudo_premier(premier):
+    if premier in (2, 3):
+        return True
+    if premier <= 1 or premier % 2 == 0:
+        return False
+
     for j in range(1,50):
-        a=random.randint(1,premier-1)
+        a=random.randint(2,premier-2)
         if temoin(a,premier):
             return False
-        else:
-            return True
+    return True
 
 
